06968826c8d0a7883214181080c53318b992885f
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2024-08-03 14:52:04 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -5,15 +5,6 @@ Jax.tunnel = {}
 function Jax:__construct()
 	vRP.Extension.__construct(self)
 	self.cfg = module("vrp_tutorial", "cfg/cfg")
-
-	RegisterCommand('id', function(source, args, rawCommand)
-		local id = self.remote.getID() -- returns the players first and last name
-		if id then
-			vRP.EXT.Base:notify("First name: "..id[2].."| Last name: "..id[2])
-		else
-			vRP.EXT.Base:notify("Something went wrong when checking your identity.")
-		end
-	end, false)
 end
 
 vRP:registerExtension(Jax)
