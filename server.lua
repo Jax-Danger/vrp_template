d580512f55b134925674fe97926a75379a20030f
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2024-08-03 14:52:17 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -6,18 +6,4 @@ function Jax:__construct()
   vRP.Extension.__construct(self)
   
 end
-function Jax:getID()
-  local user = vRP.users_by_source[source]
-  local identity = vRP.EXT.Identity:getIdentity(user.cid)
-  
-  if identity then
-    fname = identity.firstname
-    lname = identity.name
-    return {fname, lname}
-  end
-end
-Jax.tunnel.getID = Jax.getID
-
-
-
 vRP:registerExtension(Jax)
