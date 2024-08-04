19b97dfb201b0cb45ae22db9f2c49028cdbf0468
JaxDanger <jax@jaxdanger.com>
2024-08-04 12:34:06 -0400

    Revert "."
    This reverts commit edd2401b90b48a382fd1e90b3ecb4f828bd79003.

--- client.lua
+++ client.lua
@@ -4,15 +4,6 @@ Jax.tunnel = {}
 
 function Jax:__construct()
 	vRP.Extension.__construct(self)
-
-	RegisterCommand('id', function(source,args,rawCommand)
-		local id = self.remote.getID()
-		if id then
-			vRP.EXT.Base:notify('Your first name is '..id[2]..' and your last name is '..id[1])
-		else
-			vRP.EXT.Base:notify('You do not have an identity. Please relog.')
-		end
-	end, false)
 end
 
 vRP:registerExtension(Jax)
