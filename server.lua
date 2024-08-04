edd2401b90b48a382fd1e90b3ecb4f828bd79003
JaxDanger <jax@jaxdanger.com>
2024-08-04 12:33:54 -0400

    .

--- server.lua
+++ server.lua
@@ -6,4 +6,16 @@ function Jax:__construct()
   vRP.Extension.__construct(self)
 end
 
+function Jax:getID()
+  local user = vRP.users_by_source[source]
+  local identity = vRP.EXT.Identity:getIdentity(user.cid)
+
+  if identity then
+    local fname = identity.firstname
+    local lname = identity.name
+    return {fname, lname}
+  end
+end
+Jax.tunnel.getID = Jax.getID
+
 vRP:registerExtension(Jax)
